4c5fd9b562796aec2f1c374eb0be90fc209cae42
EjulianM <julian@thestonesinc.com>
2018-12-20 14:15:49 -0600

    Add files via upload

---
+++ assets/js/plugins/promo.js
@@ -0,0 +1,4 @@
+$(function() {
+  $(".fullscreen-static-image").backstretch(["assets/img-temp/promo/background_compressed.jpeg", "assets/img-temp/promo/background_compressed.jpeg"],
+  {duration: 5000, fade:1500});
+});
